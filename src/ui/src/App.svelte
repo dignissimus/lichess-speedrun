7178ad44a4655f4e18b0accdd54f90719a6301b7
Sam Ezeh <sam.z.ezeh@gmail.com>
2021-07-26 22:08:01 -0700

    Show ratings

--- src/ui/src/App.svelte
+++ src/ui/src/App.svelte
@@ -1,2 +1,28 @@
 <h1>Lichess Speedrun</h1>
 <input value="Enable" type="button"/>
+<script lang="ts">
+  import { browser } from "webextension-polyfill-ts"
+  let perfLookup: {[perf: string]: string} = {
+    "rapid": "Rapid",
+    "blitz": "Blitz",
+    "bullet": "Bullet"
+  };
+  let perfs: string[] = ["rapid", "blitz", "bullet"]
+  let ratings: {[perf: string]: (int | undefined)} = {}
+  for (let perf of perfs){
+    browser.storage.local.get(perf).then(
+      function(rating){
+        ratings[perf] = rating
+      },
+      function(error){
+        ratings[perf] = undefined
+      }
+    )
+  }
+  console.log(ratings)
+</script>
+
+{#each perfs as perf}
+  <h2>{perfLookup[perf]}</h2>
+  Rating: {ratings[perf] || "?"}
+{/each}
