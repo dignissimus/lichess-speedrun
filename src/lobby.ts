8faf6b1868af6baf5404a89878818458a6852c3a
Sam Ezeh <sam.z.ezeh@gmail.com>
2021-07-28 19:34:05 -0700

    Fix bug with hiding pools

--- src/lobby.ts
+++ src/lobby.ts
@@ -1,8 +1,26 @@
-function hidePools(){
-  [...document.getElementsByClassName("lpools")].map(element => (<HTMLElement>element).style.visibility = "hidden")
-}
-function load(){
-  hidePools()
+function pools(): HTMLElement[] {
+  return [...document.getElementsByClassName("lpools")] as HTMLElement[]
 }
 
-document.body.onload = load // window.addEventListener("load", load, true)
+function hidePools(){
+  console.log("Making an attempt at hiding")
+  pools().map(element => (<HTMLElement>element).style.visibility = "hidden")
+}
+
+
+function load(){
+  while (!pools); // Probably not a good idea
+  hidePools()
+  pools().map(pool => new MutationObserver(hidePools).observe(pool, {
+    childList: false,
+    subtree: false
+  }))
+}
+
+load()
+
+document.body.addEventListener("load", load)
+
+
+//document.body.addEventListener("load", load, true)
+// window.hidePools = hidePools
